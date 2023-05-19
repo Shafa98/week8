c47be5e4dd2f5d91df8aa35357008e0971cad9ae
Shafa98 <shahnazarovashafa@gmail.com>
2023-05-19 18:55:53 +0400

    new

---
+++ Untitled3.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Machine Learning Application')
+st.header('Welcome to ML Application')
+
+df=pd.read_csv("C:\\Users\\99451\\Downloads\\water_potability.csv")
+data=pd.read_csv("C:\\Users\\99451\\Downloads\\loan_pred.csv")
+st.dataframe(df)
+st.dataframe(data)
+
+chart2 =px.scatter(df,
+                       
+                   x='Solids',
+                   y='Hardness',
+                   color='Potability',
+                   title='Scatter Plot')
+
+st.plotly_chart(chart2)
+
+chart1=px.pie(data,
+              title='Pie Chart',
+              names='CoapplicantIncome',
+              values='Education',)
+
+st.plotly_chart(chart1)
